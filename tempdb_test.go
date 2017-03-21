d1d961bf0520f87a42ad1a184ede254eb5b358de
rafaeljesus <rafaelljesus86@gmail.com>
2017-03-21 19:11:45 -0300

    Increase coverage

--- tempdb_test.go
+++ tempdb_test.go
@@ -13,6 +13,14 @@ func TestInsert(t *testing.T) {
 	if err := temp.Insert("key", "value", 0); err != nil {
 		t.Errorf("Expected to insert key/value %s", err)
 	}
+
+	if err := temp.Insert("", "value", 0); err == nil {
+		t.Fail()
+	}
+
+	if err := temp.Insert("key", "", 0); err == nil {
+		t.Fail()
+	}
 }
 
 func TestGet(t *testing.T) {
@@ -23,6 +31,16 @@ func TestGet(t *testing.T) {
 
 	if err := temp.Insert("key", "value", 0); err != nil {
 		t.Errorf("Expected to insert key/value %s", err)
+	}
+
+	_, err = temp.Find("")
+	if err == nil {
+		t.Fail()
+	}
+
+	_, err = temp.Find("invalid_key")
+	if err == nil {
+		t.Fail()
 	}
 
 	value, err := temp.Find("key")
