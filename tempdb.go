977709ccfe500108cb891ecba6c1977ae4df12f0
rafaeljesus <rafaelljesus86@gmail.com>
2017-03-21 18:56:34 -0300

    Remove go report warnings

--- tempdb.go
+++ tempdb.go
@@ -4,7 +4,7 @@ import (
 	"crypto/tls"
 	"errors"
 	"fmt"
-	. "github.com/tj/go-debug"
+	d "github.com/tj/go-debug"
 	"gopkg.in/redis.v5"
 	"net"
 	"time"
@@ -16,7 +16,7 @@ var (
 	// ErrValueRequired is returned when value is not passed as parameter in tempdb.Config.
 	ErrValueRequired = errors.New("creating a new tempdb requires a non-empty value")
 
-	debug = Debug("single")
+	debug = d.Debug("single")
 )
 
 // Tempdb stores an expiring (or non-expiring) key/value pair in Redis.
@@ -25,12 +25,14 @@ type Tempdb interface {
 	Find(key string) (value string, err error)
 }
 
+type dialerFunc func() (net.Conn, error)
+
 // Options carries the different variables to tune a newly started redis client,
 // it exposes the same configuration available from https://godoc.org/gopkg.in/redis.v5#Options go client.
 type Options struct {
 	Network            string
 	Addr               string
-	Dialer             func() (net.Conn, error)
+	Dialer             dialerFunc
 	Password           string
 	DB                 int
 	MaxRetries         int
@@ -51,7 +53,7 @@ type temp struct {
 
 // New returns a new temp configured with the
 // variables from the options parameter, or returning an non-nil err
-// if an error ocurred while creating redis client.
+// if an error occurred while creating redis client.
 func New(o Options) (tempdb Tempdb, err error) {
 	options := newOptions(o)
 	client := redis.NewClient(options)
@@ -103,67 +105,112 @@ func (t *temp) Find(key string) (value string, err error) {
 func newOptions(o Options) (options *redis.Options) {
 	options = &redis.Options{}
 
-	if len(o.Addr) == 0 {
-		o.Addr = "localhost:6379"
-	}
-
-	options.Addr = o.Addr
-
-	if len(o.Password) != 0 {
-		options.Password = o.Password
-	}
-
-	if o.DB != 0 {
-		options.DB = o.DB
-	}
-
-	if len(o.Network) != 0 {
-		options.Network = o.Network
-	}
-
-	if o.Dialer != nil {
-		options.Dialer = o.Dialer
-	}
-
-	if o.MaxRetries != 0 {
-		options.MaxRetries = o.MaxRetries
-	}
-
-	if o.DialTimeout != 0 {
-		options.DialTimeout = o.DialTimeout
-	}
-
-	if o.ReadTimeout != 0 {
-		options.ReadTimeout = o.ReadTimeout
-	}
-
-	if o.WriteTimeout != 0 {
-		options.WriteTimeout = o.WriteTimeout
-	}
-
-	if o.PoolSize != 0 {
-		options.PoolSize = o.PoolSize
-	}
-
-	if o.PoolTimeout != 0 {
-		options.PoolTimeout = o.PoolTimeout
-	}
-
-	if o.IdleTimeout != 0 {
-		options.IdleTimeout = o.IdleTimeout
-	}
-
-	if o.IdleCheckFrequency != 0 {
-		options.IdleCheckFrequency = o.IdleCheckFrequency
-	}
-
-	if o.ReadOnly {
-		options.ReadOnly = o.ReadOnly
-	}
-
-	if o.TLSConfig != nil {
-		options.TLSConfig = o.TLSConfig
-	}
+	setAddr(options, o.Addr)
+	setPassword(options, o.Password)
+	setDB(options, o.DB)
+	setNetwork(options, o.Network)
+	setDialer(options, o.Dialer)
+	setMaxRetries(options, o.MaxRetries)
+	setDialTimeout(options, o.DialTimeout)
+	setReadTimeout(options, o.ReadTimeout)
+	setWriteTimeout(options, o.WriteTimeout)
+	setPoolSize(options, o.PoolSize)
+	setPoolTimeout(options, o.PoolTimeout)
+	setIdleTimeout(options, o.IdleTimeout)
+	setIdleCheckFrequency(options, o.IdleCheckFrequency)
+	setReadOnly(options, o.ReadOnly)
+	setTLSConfig(options, o.TLSConfig)
 
 	return
+}
+
+func setAddr(options *redis.Options, addr string) {
+	if len(addr) == 0 {
+		addr = "localhost:6379"
+	}
+	options.Addr = addr
+}
+
+func setPassword(options *redis.Options, password string) {
+	if len(password) != 0 {
+		options.Password = password
+	}
+}
+
+func setDB(options *redis.Options, db int) {
+	if db != 0 {
+		options.DB = db
+	}
+}
+
+func setNetwork(options *redis.Options, network string) {
+	if len(network) != 0 {
+		options.Network = network
+	}
+}
+
+func setDialer(options *redis.Options, dialer dialerFunc) {
+	if dialer != nil {
+		options.Dialer = dialer
+	}
+}
+
+func setMaxRetries(options *redis.Options, maxRetries int) {
+	if maxRetries != 0 {
+		options.MaxRetries = maxRetries
+	}
+}
+
+func setDialTimeout(options *redis.Options, dialTimeout time.Duration) {
+	if dialTimeout != 0 {
+		options.DialTimeout = dialTimeout
+	}
+}
+
+func setReadTimeout(options *redis.Options, readTimeout time.Duration) {
+	if readTimeout != 0 {
+		options.ReadTimeout = readTimeout
+	}
+}
+
+func setWriteTimeout(options *redis.Options, writeTimeout time.Duration) {
+	if writeTimeout != 0 {
+		options.WriteTimeout = writeTimeout
+	}
+}
+
+func setPoolSize(options *redis.Options, poolSize int) {
+	if poolSize != 0 {
+		options.PoolSize = poolSize
+	}
+}
+
+func setPoolTimeout(options *redis.Options, poolTimeout time.Duration) {
+	if poolTimeout != 0 {
+		options.PoolTimeout = poolTimeout
+	}
+}
+
+func setIdleTimeout(options *redis.Options, idleTimeout time.Duration) {
+	if idleTimeout != 0 {
+		options.IdleTimeout = idleTimeout
+	}
+}
+
+func setIdleCheckFrequency(options *redis.Options, idleCheckFrequency time.Duration) {
+	if idleCheckFrequency != 0 {
+		options.IdleCheckFrequency = idleCheckFrequency
+	}
+}
+
+func setReadOnly(options *redis.Options, readOnly bool) {
+	if readOnly {
+		options.ReadOnly = readOnly
+	}
+}
+
+func setTLSConfig(options *redis.Options, tlsConfig *tls.Config) {
+	if tlsConfig != nil {
+		options.TLSConfig = tlsConfig
+	}
 }
